3409fc37a333115a9611ce419d3d0a7398247975
Emily Schiavone <schiavone.emily@gmail.com>
2011-04-28 12:59:22 -0500

    started normal mode transformation

--- shab-pimd.cpp
+++ shab-pimd.cpp
@@ -23,6 +23,7 @@ int main (int argc, char* argv[])
   double eSteps; // Number of Equilibration Sweeps
   double rSteps; // Number of Recording Sweeps
   bool useStage; // Use Staging
+  bool useNormal; // Use Normal Mode
   bool useNH; // Use Nose-Hoover Thermostat
   int nNH; // Length of Nose-Hoover Thermostat
   int SYOrder; // Order of Suzuki-Yoshida Factorization
@@ -41,6 +42,7 @@ int main (int argc, char* argv[])
     inputStream >> eSteps;
     inputStream >> rSteps;
     inputStream >> useStage;
+    inputStream >> useNormal;
     inputStream >> useNH;
     inputStream >> nNH;
     inputStream >> SYOrder;
@@ -57,6 +59,7 @@ int main (int argc, char* argv[])
   cout << "Number of Equilibration Sweeps: " << eSteps << "\n";
   cout << "Number of Recording Sweeps: " << rSteps << "\n";
   cout << "Staging?: " << useStage << "\n";
+  cout << "Normal Mode?: " << useNormal << "\n";
   cout << "Nose-Hoover?: " << useNH << "\n";
   cout << "Nose-Hoover Length: " << nNH << "\n";
   cout << "Order of Suzuki-Yoshida Factorization: " << SYOrder << "\n";
@@ -70,16 +73,16 @@ int main (int argc, char* argv[])
 
   // Output files
   fstream scalarTrace;   
-  char scalarFormat[] = "data/traces/scalarTrace-%d-%d-%d-%g-%g-%g-%d-%d-%d-%d.dat";
+  char scalarFormat[] = "data/traces/scalarTrace-%d-%d-%d-%g-%g-%g-%g-%d-%d-%d-%d.dat";
   char scalarFile[sizeof scalarFormat+100];
-  sprintf(scalarFile,scalarFormat,nPart,nD,nBead,beta,dt,L,eSteps,rSteps,useStage,useNH,nNH,SYOrder,nNHsteps);  
+  sprintf(scalarFile,scalarFormat,nPart,nD,nBead,beta,dt,L,eSteps,rSteps,useStage,useNormal,useNH,nNH,SYOrder,nNHsteps);  
   scalarTrace.open (scalarFile, ios::out | ios::trunc);
   
   // COUT FORMATTING
   cout << scientific << setprecision(4);
     
   // Intialise paths
-  Paths path(nPart,nD,nBead,beta,dt,L,useStage,useNH,nNH,SYOrder,nNHsteps);   
+  Paths path(nPart,nD,nBead,beta,dt,L,useStage,useNormal,useNH,nNH,SYOrder,nNHsteps);   
   
   // Initialize observables;
   double PE, VE, R, R2; 
@@ -107,6 +110,7 @@ int main (int argc, char* argv[])
     
     // Verlet Step
     if (useStage) path.takeStepStage();
+    else if (useNormal) path.takeStepNormal();
     else path.takeStep();
   
     if(measureScalars && t>=eSteps) {
